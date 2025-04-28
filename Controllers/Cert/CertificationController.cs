b09d95e3cda34906f17adab28a0ca8de0d7a546b
Dagsa Adrix Llou (External) <adrix.dagsa@betterask.erni>
2025-04-28 11:28:00 +0800

    Added Error response on skill, certification, and user controller

--- Controllers/Cert/CertificationController.cs
+++ Controllers/Cert/CertificationController.cs
@@ -34,12 +34,14 @@ public class CertificationController:ControllerBase
     [ProducesResponseType(typeof(ErrorResponseDTO), 500)]
     public async Task<IActionResult> AddCertification([FromBody] CertificationsAddDTO createCertDTO){
         try{
+
             if(!await _skillRepo.SkillExistingAsync(createCertDTO.SkillId)){
                 return NotFound(new {message = "Skill not found"});
             }
             var cert = _mapper.Map<Certification>(createCertDTO);
             var newCert = await _certRepo.CreateAsync(cert);
             return Ok(_mapper.Map<CertificationsAddDTO>(newCert));
+
         }catch(Exception ex){
             return StatusCode(500, new ErrorResponseDTO
             {
@@ -51,29 +53,56 @@ public class CertificationController:ControllerBase
     }
 
     [HttpPut("trainne/{id}")]
+    [ProducesResponseType(typeof(SuccessResponseDTO), 201)]
+    [ProducesResponseType(typeof(ErrorResponseDTO), 500)]
     public async Task<IActionResult> UpdateCertification([FromRoute] int id, [FromBody] CertificationUpdateDTO certUpdateDTO){
-        if(!await _skillRepo.SkillExistingAsync(certUpdateDTO.SkillId)){
-            return NotFound(new {message = "Skill not found"});
-        }
-        var cert = await _certRepo.UpdateCertificationAsync(id, certUpdateDTO);
-        if(cert == null){
-            return NotFound(new {message = "Certification not Found"});
-        }
+        try{
+            if(!await _skillRepo.SkillExistingAsync(certUpdateDTO.SkillId)){
+                return NotFound(new {message = "Skill not found"});
+            }
+            var cert = await _certRepo.UpdateCertificationAsync(id, certUpdateDTO);
+            if(cert == null){
+                return NotFound(new {message = "Certification not Found"});
+            }
 
-        var responseDto = _mapper.Map<CertificationResponseDTO>(cert);
+            var responseDto = _mapper.Map<CertificationResponseDTO>(cert);
 
-    return Ok(responseDto);
+            return Ok(responseDto);
+
+        }catch(Exception ex){
+            return StatusCode(500, new ErrorResponseDTO{
+                Message = ex.Message,
+                Success = false,
+                StatusCode = 500
+            });
+        }
        
     }
 
     [HttpDelete("{id}")]
+    [ProducesResponseType(typeof(SuccessResponseDTO), 201)]
+    [ProducesResponseType(typeof(ErrorResponseDTO), 500)]
     public async Task<IActionResult> DeleteCertification([FromRoute] int id){
-        var certification  = await _certRepo.DeleteCertificationAsync(id);
+        try{
+            var certification  = await _certRepo.DeleteCertificationAsync(id);
 
-        if(certification == null){
-            return NotFound(new {message = "Certification Not found"});
+            if(certification == null){
+                return NotFound(new {message = "Certification Not found"});
+            }
+            
+            return Ok(new SuccessResponseDTO
+                {
+                    Message = "Certification Deleted Successfully",
+                    Success = true,
+                    StatusCode = 201
+                });
+
+        }catch(Exception ex){
+            return StatusCode(500, new ErrorResponseDTO{
+                Message = ex.Message,
+                Success = false,
+                StatusCode = 500
+            });
         }
-
-        return Ok(new {message = "Certification has been deleted"});
     }
 }
