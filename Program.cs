6666f8dd6e7f04640748d29c5071a065fe40acbc
mcronERNI <mhike.cron@betterask.erni>
2025-04-28 18:02:24 +0800

    JWT test

--- Program.cs
+++ Program.cs
@@ -61,7 +61,7 @@ builder.Services.AddMappingProfiles();
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    //options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 })
 .AddJwtBearer(options =>
 {
